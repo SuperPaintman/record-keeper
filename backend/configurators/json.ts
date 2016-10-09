1984650448f7eb61491dfb86e6c63aa317007995
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2016-10-10 03:20:40 +0500

    Added `Json` configurator

---
+++ backend/configurators/json.ts
@@ -0,0 +1,34 @@
+'use strict';
+/** Requires */
+import Configurator, {
+  IConfig
+} from './configurator';
+
+import {
+  ConfiguratorError
+} from '../libs/errors';
+
+export default class Json extends Configurator {
+  get extensions() {
+    return ['json'];
+  }
+
+  load(config: string): Promise<IConfig> {
+    let configObj: IConfig;
+
+    try {
+      configObj = JSON.parse(config);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+
+    return Promise.resolve(configObj);
+  }
+
+  formatError(err: any): ConfiguratorError {
+    return new ConfiguratorError(err.message, {
+      configurator: Json,
+      code:         err.code
+    });
+  }
+}
